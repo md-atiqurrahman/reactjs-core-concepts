23c4f5cdd5a64a2764f1e62dd6ed24f78d46bda1
md-atiqurrahman <mdatiqurrahman9104@gmail.com>
2022-09-03 11:38:59 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
-
+const singers = [
+  {name:'Arjith Singh',song: 'Tum hi ho'},
+  {name: 'James',song:'vegi vegi'},
+  {name: 'Tulsi kumar', song:'Toota hain dil mera'},
+  {name:'Darshan Raval', song:'Athi hoo toh barish lete ayna'}
+]
+const Heroes = ['Cris hemsworth','Rock','Vin disel','Jhonny deep','Shahruk Khan','Salman Khan','Aiusman Kurana','Kapil sharma']
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+         Heroes.map(hero => <li>{hero}</li>)
+      }
+      {/* {
+        Heroes.map(hero => <Person name={hero}></Person> )
+      } */}
+      {
+        singers.map(singer => <Person name={singer.name} song={singer.song}></Person>)
+      }
+      {/* <Person name={Heroes[0]} movie='Thor Rangrok'></Person>
+      <Person name={Heroes[1]} movie='Jumanji welcome to the jungle'></Person>
+      <Person name={Heroes[2]} movie='Triple x'></Person>
+      <Person name= {Heroes[3]} movie='Pirates of caribian'></Person> */}
+      <p>New component is here</p>
+      <Friend name='Soikot prodhan' job='student'></Friend>
+      <Friend name='Akter Hossain Sakib' job='Bangladesh Border gurd solider'></Friend>
     </div>
   );
 }
 
+function Person(props){
+  return (
+    <div className='person'>
+      <h1>{props.name}</h1>
+      <p>Popular song: {props.song}</p>
+    </div>
+  )
+}
+function Friend(props){
+  return (
+    <div className='container'>
+      <h1>{props.name}</h1>
+      <p>Profession: {props.job}</p>
+    </div>
+  )
+}
 export default App;
